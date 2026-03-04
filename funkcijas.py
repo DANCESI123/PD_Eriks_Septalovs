e501007c165f91e9ab22783810a7a5602d212f92
DANCESI123 <senduc3@gmail.com>
2026-03-04 14:35:03 +0200

    uzrakstita funkcijas datu ievadišana

--- funkcijas.py
+++ funkcijas.py
@@ -1,4 +1,10 @@
-ievade_datus ()
-aprekinat_materiali(Ievade_datus) 
-aprekinat_cenu(Ievade_datus, aprekinat_materiali)
-aprekinat_masu(Ievade_datus, aprekinat_materiali)
+def ievade_datus():
+    cementu_daudzums = float(input("Ievadi cementa daudzumu: "))
+    cementu_cena = float(input("Ievadi cementa cenu: "))
+    smilsu_cena = float(input("Ievadi smilšu cenu: "))
+    return cementu_daudzums, cementu_cena, smilsu_cena
+
+
+def aprekinat_materiali(Ievade_datus) 
+def aprekinat_cenu(Ievade_datus, aprekinat_materiali)
+def aprekinat_masu(Ievade_datus, aprekinat_materiali)
